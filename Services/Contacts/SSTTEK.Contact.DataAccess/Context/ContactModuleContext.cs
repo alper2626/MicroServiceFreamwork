9b6c0c6ffd2deed6a092e4c5031ecc863142ced0
abasda <alperbasda@gmail.com>
2022-10-06 22:08:49 +0300

    Contact DbContext Created. Project Referances Added.

---
+++ Services/Contacts/SSTTEK.Contact.DataAccess/Context/ContactModuleContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MsSqlAdapter.Context;
+using ServerBaseContract;
+using SSTTEK.Contacts.Entities.Db;
+
+namespace SSTTEK.Contact.DataAccess.Context
+{
+    public class ContactModuleContext : MsSqlDbContext
+    {
+        public ContactModuleContext(DatabaseOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<ContactEntity> Contacts { get; set; }
+
+        public virtual DbSet<ContactInformationEntity> ContactInformations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContactInformationEntity>().HasIndex(w => w.ContentIndex);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
